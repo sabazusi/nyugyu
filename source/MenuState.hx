933d6ae4eab3a283bccf924aafbfedba2fb25029
sabazusi <kouhei.live@gmail.com>
2015-08-12 21:31:30 +0900

    implement MenuState

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -6,12 +6,35 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.util.FlxMath;
+import flixel.util.FlxColor;
 
 class MenuState extends FlxState
 {
 	override public function create():Void
 	{
 		super.create();
+
+		this.set_bgColor(FlxColor.AZURE);
+
+		var titleSize:Int = Std.int(FlxG.width * 0.8);
+		var titleText = new FlxText(
+			(FlxG.width - titleSize) /2, 
+			100, 
+			titleSize, 
+			"NYU GYU"
+			);
+		titleText.setFormat(null, 82, FlxColor.BLACK);
+		this.add(titleText);
+
+		var navigateText = new FlxText(
+			titleText.x,
+			FlxG.height - 100,
+			FlxG.width,
+			"TAP TO START"
+			);
+		navigateText.setFormat(null, 36, FlxColor.RED);
+		this.add(navigateText);
+
 	}
 	
 	override public function destroy():Void
@@ -22,5 +45,9 @@ class MenuState extends FlxState
 	override public function update():Void
 	{
 		super.update();
+		if(FlxG.mouse.pressed)
+		{
+			FlxG.switchState(new PlayState());
+		}
 	}	
 }
